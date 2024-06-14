60a2549063c8d627de00f488dd1bb93e09f6fc51
Nikita Syzrantsev <nikita.syzrantsev@bostongene.com>
2024-06-15 00:23:13 +0400

    Update unit test

--- steps/cdr3nt_error_corrector/unit_tests/test_airr.py
+++ steps/cdr3nt_error_corrector/unit_tests/test_airr.py
@@ -4,7 +4,7 @@ import pandas as pd
 from airr import get_no_call_count, _prepare_vj_columns
 
 
-@fixture(scope="module")
+@fixture(scope="function")
 def annotation_with_no_call():
     return pd.DataFrame(
         data={
@@ -21,10 +21,20 @@ def test_get_no_call_count(annotation_with_no_call):
     assert get_no_call_count(annotation_with_no_call) == {"no_v_call": 2, "no_j_call": 2}
 
 
-def test__prepare_vj_columns(annotation_with_no_call):
+def test__prepare_vj_columns_with_best_alignment(annotation_with_no_call):
     assert _prepare_vj_columns(annotation_with_no_call, only_best_alignment=True).equals(
         pd.DataFrame(data={"v_call": ["IGHV4-59*01"],
                            "j_call": ["IGHJ4-59*01"],
+                           "v_sequence_end": [-1],
+                           "j_sequence_start": [-1]},
+                     index=[3])
+    )
+
+
+def test__prepare_vj_columns_without_best_alignment(annotation_with_no_call):
+    assert _prepare_vj_columns(annotation_with_no_call, only_best_alignment=False).equals(
+        pd.DataFrame(data={"v_call": ["IGHV4-59*01"],
+                           "j_call": ["IGHJ4-59*01,IGHJ4-59*02"],
                            "v_sequence_end": [-1],
                            "j_sequence_start": [-1]},
                      index=[3])
