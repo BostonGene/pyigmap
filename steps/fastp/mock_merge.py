44f1e2a7e8cb6efb148c70c4d5c9ec9529fa8e69
Nikita Syzrantsev <syzrantsev.n@yandex.ru>
2024-06-14 20:28:00 +0400

    Consider mock merging reads for non-overlapped reads (#86)
    * Implemented the logic with mock merging of R1 and R2 (**reverse
    complement of R2**) reads (always enabled for RNASeq bulk)
    * Added new flags `--mock-merge` and `--insert-size` to fastp step
    * Added new flag `--mock_merge_amplicon` to wf (default: false)
    * Wrote tests for fastp and mock merging
    
    ---------
    
    Co-authored-by: Nikita Syzrantsev <syzrantsev.n@yandex.ru>

---
+++ steps/fastp/mock_merge.py
@@ -0,0 +1,62 @@
+import gzip
+import itertools
+import tempfile
+
+import pyfastx
+import os
+import multiprocessing
+
+from logger import set_logger
+logger = set_logger(name=__file__)
+
+TRANSLATION_TABLE = bytes.maketrans(b"ATGCRYSWKMBDHVN", b"TACGAAAAAAAAAAA")
+
+
+def get_fastq_reads(fq1_path: str, fq2_path: str) -> tuple[list[str], list[str]]:
+    """Reads a pair of FASTQ files into two lists of reads"""
+    logger.info("Reading FASTQ files...")
+
+    reads1 = [seq for seq in pyfastx.Fastq(fq1_path, build_index=False, full_name=True)]
+    reads2 = [seq for seq in pyfastx.Fastq(fq2_path, build_index=False, full_name=True)]
+
+    logger.info("FASTQ files have been read.")
+
+    return reads1, reads2
+
+
+def save_fastq_reads_to_file(reads: list[str], output_path: str):
+    """Saves processed reads to gzipped file"""
+    with gzip.open(output_path, "wb") as f:
+        for read in reads:
+            f.write(read.encode("utf-8"))
+
+
+def get_reverse_complement(read_sequence: str) -> str:
+    """Returns reverse complement of nucleotide sequence"""
+    return read_sequence.translate(TRANSLATION_TABLE)[::-1]
+
+
+def mock_merge_one_reads_pair(read1: list[str], read2: list[str], insert_size: int) -> str:
+    """Perform a mock merging for non-overlapping reads"""
+    new_header = f"{read1[0]} mock_merged_{len(read1[1])}_{len(read2[1])}"
+    new_read_sequence = read1[1] + "N" * insert_size + get_reverse_complement(read2[1])
+    new_read_quality = read1[2] + "#" * insert_size + read2[2]
+    return f"@{new_header}\n" \
+           f"{new_read_sequence}\n" \
+           f"+\n" \
+           f"{new_read_quality}\n"
+
+
+def mock_merge_reads(fq1_path: str, fq2_path: str, insert_size) -> str:
+    """Run mock merging in parallel"""
+    reads1, reads2 = get_fastq_reads(fq1_path, fq2_path)
+
+    with multiprocessing.Pool(processes=os.cpu_count()) as pool:
+        args_list = zip(reads1, reads2, itertools.repeat(insert_size, len(reads1)))
+        processed_reads = pool.starmap(mock_merge_one_reads_pair, args_list)
+
+    merged_fq_path = tempfile.NamedTemporaryFile().name
+
+    save_fastq_reads_to_file(processed_reads, merged_fq_path)
+
+    return merged_fq_path
--- steps/fastp/run.py
+++ steps/fastp/run.py
@@ -1,20 +1,25 @@
 import argparse
-import logging
+import gzip
+import shutil
 import subprocess
 import os
 import sys
+import tempfile
+
+from mock_merge import mock_merge_reads
+
+from logger import set_logger
+
+logger = set_logger(name=__file__)
 
 
-LOGGER_FORMAT = "%(name)s | line %(lineno)-3d | %(levelname)-8s | %(message)s"
-logger = logging.getLogger(name=__file__)
-
-
-def set_logger(logger_format: str = LOGGER_FORMAT) -> None:
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.INFO)
-    handler.setFormatter(logging.Formatter(logger_format))
-    logger.setLevel(logging.INFO)
-    logger.addHandler(handler)
+def check_argument_consistency(args: argparse.Namespace) -> list[str]:
+    msg_list = []
+    if args.insert_size and not args.mock_merge:
+        msg_list += ["--insert-size cannot be provided without --mock-merge"]
+    if args.mock_merge and (args.out_fq1 or args.out_fq2):
+        msg_list += ["--out-fq1 or --out-fq2 cannot be used with --mock-merge"]
+    return msg_list
 
 
 def parse_args() -> argparse.Namespace:
@@ -23,15 +28,50 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('--in-fq2', help='Input fastq.gz file, PE pair 2')
     parser.add_argument('--trimq',
                         help='Reads trimming: minimal base quality for 5'' & 3'' read ends to keep them', type=int)
-    parser.add_argument('--disable', help='Mode(s) to disable', nargs='+', action='extend')
+    parser.add_argument('--disable', help='Mode(s) to disable',
+                        choices=["length_filtering", "quality_filtering", "adapter_trimming"], nargs='+', action='extend')
     parser.add_argument('--merge', help='Merge reads', action='store_true')
+    parser.add_argument('--mock-merge', help='Enable mock merging of reads', action='store_true')
+    parser.add_argument('--insert-size', help='Insert size for mock merging', type=int)
     parser.add_argument('--out-fq1', help='Output fastq file, SE or PE pair 1')
     parser.add_argument('--out-fq2', help='Output fastq file, PE pair 2')
     parser.add_argument('--out-fq12', help='Output merged fastq file, PE pairs 1 and 2')
     parser.add_argument('--out-html', help='Output html file', required=True)
     parser.add_argument('--out-json', help='Output json file', required=True)
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    error_message_list = check_argument_consistency(args)
+    if error_message_list:
+        parser.error("\n".join(error_message_list))
+
+    return args
+
+
+def replace_file(src_file: str, dst_file: str):
+    """
+    Replaces file from source to the destination
+
+    :param src_file: path to the file, that we need to move
+    :param dst_file: path to destination file
+    """
+    shutil.move(src_file, dst_file)
+    logger.info(f'{src_file} replaced to {dst_file}')
+
+
+def check_if_exist(file: str):
+    """
+    Checks if file exists
+
+    :param file: path to the file
+    """
+    if not os.path.exists(file):
+        logger.critical(f"Expected file {file} not found, exiting...")
+        sys.exit(1)
+    if os.path.getsize(file) == 0:
+        logger.critical(f'Output file {file} is an empty, exiting..')
+        sys.exit(1)
+    logger.info(f"Expected file {file} found.")
 
 
 def run_command(command: list[str], stdin=None):
@@ -52,27 +92,64 @@ def run_command(command: list[str], stdin=None):
     logger.info(command_process.stderr)
 
 
-def run(args: argparse.Namespace) -> None:
+def run_fastp(in_fq1: str, in_fq2: str, trimq: int, disable: str, merge: bool, out_json: str, out_html: str,
+              out_fq1: str, out_fq2: str, out_fq12: str):
+    cmd = ['fastp', '-i', in_fq1]
+    cmd += ['-o', out_fq1]
 
-    cmd = ['fastp', '-i', args.in_fq1]
-    cmd += ['-o', args.out_fq1]
+    if in_fq2:
+        cmd += ['-I', in_fq2, '-O', out_fq2, '--detect_adapter_for_pe']
 
-    if args.in_fq2:
-        cmd += ['-I', args.in_fq2, '-O', args.out_fq2, '--detect_adapter_for_pe']
+    if merge:
+        cmd += ['--merge', '--merged_out', out_fq12]
 
-    if args.merge:
-        cmd += ['--merge', '--merged_out', args.out_fq12]
-
-    cmd += ['--cut_tail', '--cut_front', '--cut_window_size', str(1), '--cut_mean_quality', str(args.trimq)] if args.trimq else []
-    cmd += [f'--disable_{mode}' for mode in args.disable] if args.disable else []
-    cmd += ['--thread', str(os.cpu_count()), '--html', args.out_html, '--json', args.out_json]
+    cmd += ['--cut_tail', '--cut_front', '--cut_window_size', str(1), '--cut_mean_quality',
+            str(trimq)] if trimq else []
+    cmd += [f'--disable_{mode}' for mode in disable] if disable else []
+    cmd += ['--thread', str(os.cpu_count()), '--html', out_html, '--json', out_json]
 
     run_command(cmd)
 
 
-if __name__ == "__main__":
-    set_logger(LOGGER_FORMAT)
+def concat_gz_files(gz_files: list[str]) -> str:
+    """Concatenates gz files into one file"""
+    out_file_path = tempfile.NamedTemporaryFile(suffix=".gz").name
+    with gzip.open(out_file_path, "ab") as concat_file:
+        for gz_file in gz_files:
+            with gzip.open(gz_file, "rb") as f:
+                concat_file.write(f.read())
+                logger.info(f"{gz_file} appended to {out_file_path}")
+    check_if_exist(out_file_path)
+    logger.info(f"{out_file_path} concatenation has been done.")
+    return out_file_path
 
+
+def run(args: argparse.Namespace) -> None:
+
+    merge = True if args.mock_merge else args.merge
+
+    out_fq1 = tempfile.NamedTemporaryFile(suffix=".gz").name
+    out_fq2 = tempfile.NamedTemporaryFile(suffix=".gz").name
+    out_fq12 = tempfile.NamedTemporaryFile(suffix=".gz").name
+
+    run_fastp(args.in_fq1, args.in_fq2, args.trimq, args.disable, merge, args.out_json, args.out_html,
+              out_fq1, out_fq2, out_fq12)
+
+    if args.mock_merge and os.path.getsize(out_fq1) != 0 and os.path.getsize(out_fq2) != 0:
+        fq12_mock = mock_merge_reads(out_fq1, out_fq2, args.insert_size)
+        out_fq12 = concat_gz_files([out_fq12, fq12_mock])
+
+    if args.out_fq1:
+        replace_file(out_fq1, args.out_fq1)
+
+    if args.out_fq2:
+        replace_file(out_fq2, args.out_fq2)
+
+    if args.out_fq12:
+        replace_file(out_fq12, args.out_fq12)
+
+
+if __name__ == "__main__":
     args = parse_args()
     logger.info(f"Starting program with the following arguments: {vars(args)}")
 
