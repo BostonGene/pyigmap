6af777b9750f41b0a7c5137025e6fa7e76905e24
Elizaveta Vlasova <vlasova.elizaveta@yandex.ru>
2025-04-03 17:43:40 +0300

    umi report code

---
+++ bin/reporter/run.py
@@ -0,0 +1,57 @@
+import argparse
+
+import pandas as pd
+from logger import set_logger
+
+
+from bin.reporter.utils import get_read_to_umi_mapping, get_consensus_group_size_per_read
+from bin.reporter.viz import create_report
+
+logger = set_logger(name=__file__)
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--in-fq1-pyumi', help='Input fastq.gz file after pyumi step, SE or PE pair 1',
+                        required=True)
+    parser.add_argument('--in-fq2-pyumi', help='Input fastq.gz file after pyumi step, PE pair 2')
+
+    parser.add_argument('--in-fq1-calib', help='Input fastq.gz file after calib step, SE or PE pair 2',
+                        required=True)
+    parser.add_argument('--in-fq2-calib', help='Input fastq.gz file after calib step, PE pair 2')
+
+    parser.add_argument('--umi-reverse', action='store_true')
+
+    parser.add_argument('--report-file')
+
+    return parser.parse_args()
+
+
+def main() -> None:
+    args = parse_args()
+    logger.info(f"Starting program with the following arguments: {args}")
+
+    pyumi_data_path = args.in_fq1_pyumi if not args.umi_reverse else args.in_fq2_pyumi
+    calib_data_path = args.in_fq1_calib if not args.umi_reverse else args.in_fq2_calib
+
+    logger.info(f"Started reading initial data from {pyumi_data_path}")
+    umi_to_count_mapping_pre, id_to_umi, sequences = get_read_to_umi_mapping(pyumi_data_path)
+
+    logger.info(f"Started reading calib data from {calib_data_path}")
+    umi_to_count_mapping_post = get_consensus_group_size_per_read(calib_data_path, id_to_umi)
+
+    logger.info(f"Created dataset")
+    res = pd.DataFrame({'umi': umi_to_count_mapping_pre.keys(),
+                        'read_num_pre_dedup': umi_to_count_mapping_pre.values()}).merge(
+        pd.DataFrame({'umi': umi_to_count_mapping_post.keys(),
+                      'read_num_post_dedup': umi_to_count_mapping_post.values()}), how='outer'
+    ).fillna(0)
+
+    res['umi'] = res['umi'].apply(lambda x: x.decode('ascii'))
+
+    logger.info('Started report creation...')
+    create_report(res, sequences, report_file_name=args.report_file)
+    logger.info(f'Report saved successfully in {args.report_file}.')
+    
+
+if __name__ == "__main__":
+    main()
---
+++ bin/reporter/viz.py
@@ -0,0 +1,157 @@
+
+import panel as pn
+import matplotlib.pyplot as plt
+import seaborn as sns
+import logomaker
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+import numpy as np
+from collections import Counter
+from bokeh.plotting import figure
+from bokeh.models import HoverTool
+# from bokeh.palettes import Category10
+
+
+def compute_logo_matrix(sequences):
+    seq_len = len(sequences[0])
+    counts = {i: Counter() for i in range(seq_len)}
+    for seq in sequences:
+        for i, char in enumerate(seq):
+            counts[i][char] += 1
+    freq_df = pd.DataFrame.from_dict({
+        i: {char: count / sum(counts[i].values()) for char, count in counts[i].items()}
+        for i in counts
+    }, orient='index').fillna(0)
+    return freq_df
+
+
+def plotly_logo(sequences):
+    freq_df = compute_logo_matrix(sequences)
+    fig = go.Figure()
+    y_offset = np.zeros(len(freq_df))
+    for letter in sorted(freq_df.columns, key=lambda x: -freq_df[x].sum()):
+        heights = freq_df[letter].values
+        hovertext = [f"{letter}: {100*h:.1f}%" for h in heights]
+        writtentext = [letter if h > 0.05 else '' for h in heights]
+        fig.add_trace(go.Bar(
+            x=list(freq_df.index),
+            y=heights,
+            base=y_offset,
+            hovertext=hovertext,
+            hoverinfo='text',
+            text=writtentext,
+            name=letter,
+            marker=dict(line=dict(width=0)),
+            width=0.9
+        ))
+        y_offset += heights
+    fig.update_layout(
+        barmode='stack',
+        title='Sequence Logo (Interactive)',
+        xaxis_title='Position',
+        yaxis_title='Frequency',
+        xaxis=dict(tickmode='linear'),
+        yaxis=dict(range=[0, 1]),
+        showlegend=False,
+        height=400
+    )
+    return fig
+
+
+def bokeh_histogram(res, plot_by='read_num_pre_dedup', bins=20):
+    data = res[plot_by]
+    hist, edges = np.histogram(data, bins=np.logspace(np.log10(1), np.log10(data.max() + 1), bins))
+    p = figure(
+        title=f"UMI Usage ({plot_by})",
+        x_axis_type="log",
+        x_axis_label='Read Count',
+        y_axis_label='Number of UMIs',
+        height=500,
+        width=600,
+        tools="pan,wheel_zoom,box_zoom,reset,save"
+    )
+    p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
+           fill_color="skyblue", line_color="black")
+    p.add_tools(HoverTool(tooltips=[("Bin", "@left - @right"), ("Count", "@top")], mode="mouse"))
+    return p
+
+
+def plot_umi_scatter(res):
+    res = res.copy()
+    res = res[res['read_num_pre_dedup'] > 0]
+    res['delta'] = res['read_num_post_dedup'] / res['read_num_pre_dedup']
+    top10 = res.sort_values('delta', ascending=False).head(int(len(res) * 0.1))
+    fig = px.scatter(
+        top10,
+        x="read_num_pre_dedup",
+        y="read_num_post_dedup",
+        hover_name="umi",
+        log_x=True,
+        log_y=True,
+        title="Top 10% UMI Usage Increase After Deduplication",
+        labels={
+            "read_num_pre_dedup": "Pre-dedup Read Count",
+            "read_num_post_dedup": "Post-dedup Read Count"
+        }
+    )
+    fig.update_traces(marker=dict(size=6, color='darkorange'))
+    return fig
+
+
+def bokeh_weighted_usage(res, col='read_num_pre_dedup', bins=20):
+    df = res.copy()
+    total_reads = df[col].sum()
+    df['weight'] = df[col] / total_reads
+    values = df[col].values
+    weights = df['weight'].values
+    hist, edges = np.histogram(values, bins=np.logspace(np.log10(1), np.log10(values.max() + 1), bins), weights=weights)
+    p = figure(
+        title=f"Weighted UMI Usage ({col})",
+        x_axis_type="log",
+        x_axis_label='Read Count',
+        y_axis_label='Weighted UMI Frequency',
+        height=500,
+        width=600,
+        tools="pan,wheel_zoom,box_zoom,reset,save"
+    )
+    p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:], fill_color="skyblue", line_color="black")
+    p.add_tools(HoverTool(tooltips=[("Bin", "@left - @right"), ("Weighted Freq", "@top")], mode="mouse"))
+    return p
+
+
+def create_report(res, umi_sequences, report_file_name='panel_report.html'):
+    tab_logo = pn.Column(
+        pn.pane.Markdown("## Sequence Logo"),
+        pn.pane.Plotly(plotly_logo([x[:12].decode('ascii') for x in umi_sequences]), config={'responsive': True})
+    )
+    tab_umi_usage = pn.Column(
+        pn.pane.Markdown("## UMI Usage Pre- and Post-Deduplication"),
+        pn.Row(
+            pn.pane.Bokeh(bokeh_histogram(res, plot_by='read_num_pre_dedup')),
+            pn.pane.Bokeh(bokeh_histogram(res, plot_by='read_num_post_dedup'))
+        )
+    )
+    tab_scatter = pn.Column(
+        pn.pane.Markdown("## Top 10% UMI Usage Increase (Scatterplot)"),
+        pn.pane.Plotly(plot_umi_scatter(res), config={'responsive': True})
+    )
+    tab_weighted = pn.Column(
+        pn.pane.Markdown("## Weighted UMI Usage by Read Count"),
+        pn.Row(
+            pn.pane.Bokeh(bokeh_weighted_usage(res, col='read_num_pre_dedup')),
+            pn.pane.Bokeh(bokeh_weighted_usage(res, col='read_num_post_dedup'))
+        )
+    )
+    tabs = pn.Tabs(
+        ("Sequence Logo", tab_logo),
+        ("UMI Usage", tab_umi_usage),
+        ("Weighted UMI Usage", tab_weighted),
+        ("UMI Usage Changes", tab_scatter)
+
+    )
+    report = pn.template.FastListTemplate(
+        title="UMI Analysis Report",
+        main=[tabs]
+    )
+    report.save(report_file_name)
